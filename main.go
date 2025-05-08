3605d3478f72f0afc8d88d68af18e0eaf14655e2
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-05-08 23:28:39 +0530

    feat: Add root endpoint to check and return API status

--- main.go
+++ main.go
@@ -11,5 +11,9 @@ func main() {
 	fmt.Println("Building a Todo app with React and Go")
 	app := fiber.New()
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"message": "Go Todo App"})
+	})
+
 	log.Fatal(app.Listen(":4000"))
 }
